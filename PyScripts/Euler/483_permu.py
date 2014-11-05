d7571cefaa17d9ef648bdd0aef8dafbbda72ac7a
sarathm09 <sarathm09@gmail.com>
2014-11-06 01:17:13 +0530

    Euler : 11/6

--- PyScripts/Euler/483_permu.py
+++ PyScripts/Euler/483_permu.py
@@ -2,44 +2,50 @@ __author__ = 'T90'
 __version__ = '1.0.0'
 
 
-orig = (1, 2, 3, 4, 5)
-
-
-def diff(a):
-	c = 0
-	for i in range(0, len(a)):
-		if a[i] != orig[i]:
-			c += 1
-	if c == 0:
-		c = 1
-	return c
-
 
 def rotate(strg, n):
 	return strg[n:] + strg[:n]
 
-def permute(a):
-	print a
-	t = ''
-	if len(a) == 3:
-		t = a[0]
-		a[0] = t
-		print a
-	# 	t, a[0], a[2] = a[0], a[2], t
-	# 	print a
-	# 	t, a[1], a[2] = a[1], a[2], t
-	# 	print a
-	# 	rotate(a,1)
-	# 	print a
-	# 	rotate(a,-1)
-	# 	print a
-	else:
-		permute(a[1:])
 
+def swap(i, j, c):
+	c = list(c)
+	c[i-1], c[j-1] = c[j-1], c[i-1]
+	return ''.join(c)
+
+count = 0
+def val(s, t):
+	global count
+	count += 1
+	print count, t + s
+
+
+def six_steps(s, t = ''):
+	val(s, t)
+	val(swap(1, 2, s), t)
+	val(swap(1, 3, s), t)
+	val(swap(2, 3, s), t)
+	val(rotate(s, 1), t)
+	val(rotate(s, -1), t)
+
+
+def permute(s):
+	n = 4
+	if len(s) == 3:
+		six_steps(s)
+	else:
+		while n != len(s)+1:
+			p = -1 * n
+			print p
+			sub, rest = s[p:], s[:p]
+			for i in range(0, n):
+				t = rotate(sub, i)
+				six_steps(t[-3:], rest+t[: -3])
+			n += 1
 
 
 def main():
-	permute('123')
+	a = "abcde"
+	permute(a)
 
 
 if __name__ == '__main__':
