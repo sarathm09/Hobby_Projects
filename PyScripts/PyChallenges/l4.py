d5ad007b37e4edd764aeef9f94c3f2370ccfd0cc
sarathm09 <sarathm09@gmail.com>
2014-11-09 16:15:39 +0530

    l4_v1

---
+++ PyScripts/PyChallenges/l4.py
@@ -0,0 +1,18 @@
+__author__ = 'T90'
+__version__ = '1.0.0'
+
+import urllib
+import re
+
+nothing = '82682'
+
+nothin = re.compile('nothing is ([0-9]*)')
+
+
+while nothing.isdigit():
+	txt = urllib.urlopen('http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=' + nothing).read()
+	n = nothin.findall(txt)
+	nothing = n[0]
+	print n, nothing
+	if nothing == '16044':
+		nothing = str(int(nothing)/2)
