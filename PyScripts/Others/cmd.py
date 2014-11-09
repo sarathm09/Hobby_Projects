38a20cd383cc383fc6a9b4d0a0ead9e3d499b5c0
sarathm09 <sarathm09@gmail.com>
2014-11-09 19:31:16 +0530

    Compile with bcc added

--- PyScripts/Others/cmd.py
+++ PyScripts/Others/cmd.py
@@ -3,8 +3,28 @@ __version__ = '1.0.0'
 
 from subprocess import call
 import os
+import re
 
-a = os.popen("bcc32 a.c").read()
+fileName = "a.c"
 
-print a
+
+def compile():
+	a = os.popen("bcc32 " + fileName).read()
+	errors = ["Could not find file", "Statement missing", "errors in Compile"]
+	for error in errors:
+		if error in a:
+			print 'Please Check your file; The reported Errors are : \n'
+			er = re.findall('Error .* ' + fileName + ' ([0-9]+: .*)\n', a)
+			for e in er:
+				print e
+			return False
+	return True
+
+
+def run():
+	print 'Program Successfully compiled!!!. Here is the result : \n\n'
+	os.system(fileName.replace(".c", ""))
+
+if compile():
+	run()
 
