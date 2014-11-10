0416a7e77a99fa662aa1cb81b725a52bb21eab78
sarathm09 <sarathm09@gmail.com>
2014-11-10 12:21:41 +0530

    pSHC_dev

--- PyScripts/SHC_reborn/test.py
+++ PyScripts/SHC_reborn/test.py
@@ -2,10 +2,23 @@ __author__ = 'T90'
 __version__ = '1.0.0'
 
 import json
+import re
 
-fp = open("dict.json", "r")
+fp = open("data/dict.json", "r")
 j = json.load(fp)
-for a in j:
-	print "\n Module " + a + " : "
-	for b in str(j[a]).split(','):
-		print b,", "
+# for a in j:
+# 	print "\n Module " + a + " : "
+# 	for b in str(j[a]).split(','):
+# 		print b,", ",
+
+word = raw_input("\nWord : ")
+while word != "exit":
+	for a in j:
+		if word in j[a]:
+			temp = open("data/" + a + ".txt").read()
+			word = word.replace(" ", "").replace("\n", "")
+			reg = "##" + word + "##\n" + "(.*)" + "\n##" + word + "##"
+			code = re.findall(reg, temp, re.MULTILINE | re.DOTALL)
+			print code[0]
+			break
+	word = raw_input("\nWord : ")
