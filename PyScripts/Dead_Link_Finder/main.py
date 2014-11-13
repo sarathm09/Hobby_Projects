bb6f5d565729800590db27ecfbc5800fa6c03a4e
sarathm09 <sarathm09@gmail.com>
2014-11-14 00:09:28 +0530

    d_link

--- PyScripts/Dead_Link_Finder/main.py
+++ PyScripts/Dead_Link_Finder/main.py
@@ -1,3 +1,22 @@
 __author__ = 'T90'
 __version__ = '1.0.0'
 
+import getdata
+
+if __name__ == '__main__':
+	starturl = "http://csebeta.x10.mx"
+	links = [starturl]
+	visited = []
+	dead = []
+	while len(links) > 0:
+		try:
+			if starturl in links[0]:
+				links += getdata.fetch_urls(links[0])
+				for a in links:
+					print a
+				visited.append(links[0])
+			links.pop(0)
+		except:
+			dead.append(links[0])
+			links.pop(0)
+	print 'Dead : ', dead
