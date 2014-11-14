691b7f1d1ff2b3bb6cb61c7956bfe6123d7df4e4
sarathmsmenon <sarathm09@gmail.com>
2014-11-14 22:01:49 +0530

    yahoo_search

--- PyScripts/Scrapping_Experiments/Search/YahooSearch.py
+++ PyScripts/Scrapping_Experiments/Search/YahooSearch.py
@@ -10,32 +10,32 @@ import re
 
 
 def searchhtml(keyword):
-	chrome = mechanize.Browser()
-	chrome.set_handle_robots(False)
-	chrome.addheaders = [('User-agent',
-						  'Mozilla/5.0 (Windows NT 6.1; rv:33.0) Gecko/20100101 Firefox/33.0')]
+	moz = mechanize.Browser()
+	moz.set_handle_robots(False)
+	moz.addheaders = [('User-agent',
+						  'Mozilla/5.0 (Windows NT 6.2; rv:33.0) Gecko/20100101 Firefox/33.0')]
 
 	base_url = "https://search.yahoo.com/search?p="
 	search_url = base_url + keyword.replace(' ', '+')
 
-	return chrome.open(search_url).read() + chrome.open(search_url + "&b=11").read() + \
-		   chrome.open(search_url + "&b=21").read()
+	return moz.open(search_url).read() + moz.open(search_url + "&b=11").read() + \
+			moz.open(search_url + "&b=21").read()
 
 
 def getlinks(keyword):
 	results = []
 	html = searchhtml(keyword)
-	bs = BeautifulSoup(html).findAll('li', attrs={'class': 'g'})
-	linkre = re.compile('<a.*href="([^"]*)".*>.*</a>')
-	h3re = re.compile('<a.*>(.*)</a>')
+	bs = BeautifulSoup(html).findAll('div', attrs={'class': 'res'})
+	linkre = re.compile('<a.*href="(.*)".*>.*</a>')
+	h3re = re.compile('<a[^>]*>(.*)</a>')
 	for a in bs:
 		item = str(BeautifulSoup(str(a)).find('h3'))
-		results.append([re.findall(h3re, item)[0], re.findall(linkre, item)[0]])
+		results.append([str(re.findall(h3re, item)[0]).replace('<b>', '').replace('</b>', ''), re.findall(linkre, item)[0]])
 	return results
 
 
 if __name__ == '__main__':
 	results = getlinks("Google")
-	print "Top " + str(len(results)) + " results from Google\n"
+	print "Top " + str(len(results)) + " results from Yahoo\n"
 	for result in results:
 		print result[0] + ", " + result[1]
