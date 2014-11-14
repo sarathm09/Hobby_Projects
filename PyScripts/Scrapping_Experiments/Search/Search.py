553b3b74cba155b4a4042a0a1de6e5f7df833cfc
sarathmsmenon <sarathm09@gmail.com>
2014-11-14 22:44:28 +0530

    Search

--- PyScripts/Scrapping_Experiments/Search/Search.py
+++ PyScripts/Scrapping_Experiments/Search/Search.py
@@ -1,14 +1,23 @@
 __author__ = 'T90'
 __version__ = '1.0.0'
 
+from threading import Thread
 import GoogleSearch as goog
 import YahooSearch as yahoo
 import BingSearch as bing
-from
 
 if __name__ == '__main__':
+
 	keyword = raw_input("Enter the search key : ")
 	num = input("How many pages should I search? ")
-	goog.display_results(keyword, num)
-	bing.display_results(keyword, num)
-	yahoo.display_results(keyword, num)
+	gTH = Thread(target=goog.display_results, args=(keyword, num))
+	bTH = Thread(target=bing.display_results, args=(keyword, num))
+	yTH = Thread(target=yahoo.display_results, args=(keyword, num))
+
+	Th = [gTH, bTH, yTH]
+
+	for t in Th:
+		t.run()
+
+	for t in Th:
+		t.join()
