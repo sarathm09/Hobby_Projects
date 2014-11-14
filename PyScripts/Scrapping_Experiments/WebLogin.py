255cf58a3424cbf1d7f133ddecc801ac91a0430f
sarathmsmenon <sarathm09@gmail.com>
2014-11-15 01:26:42 +0530

    WebLogin_prealphav1

--- PyScripts/Scrapping_Experiments/WebLogin.py
+++ PyScripts/Scrapping_Experiments/WebLogin.py
@@ -1,35 +1,48 @@
 __author__ = 'T90'
 __version__ = '1.0.0'
 
+import re
 import mechanize
-from BeautifulSoup import BeautifulSoup
 import HTMLParser
+from BeautifulSoup import BeautifulSoup
 
-def getdata():
+
+def getdata(id, passw):
 	cols = [2, 3, 4, 8, 9, 12]
 	browser = mechanize.Browser(factory=mechanize.RobustFactory())
 	browser.set_handle_robots(False)
 	r = browser.open("http://rajagiritech.ac.in/stud/parent/")
 	browser.select_form(nr=0)
-	browser.form["user"] = "U1103121"
-	browser.form["pass"] = "3788"
+	browser.form["user"] = id
+	browser.form["pass"] = passw
 	print 'Submitting Auth'
 	browser.submit()
 	if 'Parent.asp' in str(browser.geturl()):
 		print 'Auth Success'
-		marklistpage = HTMLParser.HTMLParser().unescape(browser.open('http://rajagiritech.ac.in/stud/parent/Stud_View.asp?Sem=S7'))
+		marklistpage = browser.open('http://rajagiritech.ac.in/stud/parent/Stud_View.asp?Sem=S7')
 		bs = BeautifulSoup(marklistpage)
 		print 'Marklist loaded'
+		namere = re.compile('Academic details of <b>([.*])on S7</b>')
+		print re.search(namere, str(marklistpage))
+		name = str(re.findall(namere, str(marklistpage))[0])
+		print '\n Marklist of ' + name + '\n'
 		table = bs.findAll('table', attrs={"class": "ibox"})[0]
 		rownum = 1
 		for row in table.findAll('tr'):
 			if rownum > 2:
 				td = row.findAll('td')
 				colnum = 1
+				print '\n'
 				for data in td:
 					if colnum in cols:
-						print data.text
+						text = HTMLParser.HTMLParser().unescape(data.text)
+						text = text + "\t\t" if len(text) < 13 else text[:12] + "..."
+						print text, "\t",
 					colnum += 1
 			rownum += 1
 
-getdata()
+
+if __name__ == '__main__':
+	idnum = input("last 4 digits : ")
+	passw = (3774 + (idnum - 3107))
+	getdata("U110" + str(idnum), str(passw))
