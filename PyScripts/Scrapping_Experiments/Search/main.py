f08869c299fe5f4f24da1867f97891be22553b8a
sarathmsmenon <sarathm09@gmail.com>
2014-11-15 00:07:04 +0530

    Search_v1.1

--- PyScripts/Scrapping_Experiments/Search/main.py
+++ PyScripts/Scrapping_Experiments/Search/main.py
@@ -7,7 +7,15 @@ import YahooSearch as yahoo
 import BingSearch as bing
 
 if __name__ == '__main__':
+	"""
+	This program is designed to print the title and links of the top results corresponding to the search keyword,
+	separately from Google, Yahoo and Bing.
 
+	keyword : the word to be searched
+	num : How many pages of Google, Yahoo and Bing needs to be searched. The larger the value, the larger the number of
+			results, and also larger the time and network usage
+
+	"""
 	keyword = raw_input("Enter the search key : ")
 	num = input("How many pages should I search? ")
 	gTH = Thread(target=goog.display_results, args=(keyword, num))
