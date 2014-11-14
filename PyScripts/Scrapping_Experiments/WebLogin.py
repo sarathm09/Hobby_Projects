d98491e439c28178c0b7410d591117403d449036
sarathmsmenon <sarathm09@gmail.com>
2014-11-15 00:46:08 +0530

    WebLogin_prealpha

--- PyScripts/Scrapping_Experiments/WebLogin.py
+++ PyScripts/Scrapping_Experiments/WebLogin.py
@@ -3,11 +3,10 @@ __version__ = '1.0.0'
 
 import mechanize
 from BeautifulSoup import BeautifulSoup
-import re
+import HTMLParser
 
 def getdata():
 	cols = [2, 3, 4, 8, 9, 12]
-	datare = re.compile('>([A-Za-z0-9]*<)')
 	browser = mechanize.Browser(factory=mechanize.RobustFactory())
 	browser.set_handle_robots(False)
 	r = browser.open("http://rajagiritech.ac.in/stud/parent/")
@@ -18,17 +17,19 @@ def getdata():
 	browser.submit()
 	if 'Parent.asp' in str(browser.geturl()):
 		print 'Auth Success'
-		bs = BeautifulSoup(browser.open('http://rajagiritech.ac.in/stud/parent/Stud_View.asp?Sem=S7'))
+		marklistpage = HTMLParser.HTMLParser().unescape(browser.open('http://rajagiritech.ac.in/stud/parent/Stud_View.asp?Sem=S7'))
+		bs = BeautifulSoup(marklistpage)
 		print 'Marklist loaded'
 		table = bs.findAll('table', attrs={"class": "ibox"})[0]
+		rownum = 1
 		for row in table.findAll('tr'):
-			td = row.findAll('td')
-			colnum = 1
-			for data in td:
-				#if colnum in cols:
-				print data
-				print data.text
-				print re.findall(datare, str(data))[0]
-				colnum += 1
+			if rownum > 2:
+				td = row.findAll('td')
+				colnum = 1
+				for data in td:
+					if colnum in cols:
+						print data.text
+					colnum += 1
+			rownum += 1
 
 getdata()
