3ed9499534d39ef55f140e986e8fcd8f6750015c
sarathmsmenon <sarathm09@gmail.com>
2014-11-16 10:23:04 +0530

    rsetpass

---
+++ PyScripts/RSMS-profile/main.py
@@ -0,0 +1,51 @@
+__author__ = 'T90'
+__version__ = '1.0.0'
+
+__author__ = 'T90'
+__version__ = '1.0.0'
+
+import mechanize
+from BeautifulSoup import BeautifulSoup
+
+database = []
+
+
+def getdata(id, passw):
+	global database
+	te = []
+	cols = [2, 3, 4, 8, 9, 12]
+	browser = mechanize.Browser()
+	browser.set_handle_robots(False)
+	browser.open("http://rajagiritech.ac.in/stud/parent/")
+	browser.select_form(nr=0)
+	browser.form["user"] = id
+	browser.form["pass"] = passw
+	browser.submit()
+	if 'Parent.asp' in str(browser.geturl()):
+		#print 'Auth Success'
+		marklistpage = browser.open('http://rajagiritech.ac.in/stud/parent/stud_verify.asp')
+		html = str(marklistpage.read()).encode('utf-8')
+		bs = BeautifulSoup(html)
+		for inp in bs.findAll('input', attrs={"disabled": "disabled"}):
+			te.append(str(inp['value']))
+		print te[1].replace('.', '')
+		database.append(te)
+
+
+def tocsv(data):
+	f = open('details.csv', 'a')
+	for student in data:
+		for details in student:
+				f.write(details + ", ")
+		f.write("\n")
+	f.close()
+
+
+if __name__ == '__main__':
+	data = open('p.txt', 'r').read().split('\n')
+	for temp in data:
+		stud = temp.split(',')
+		idnum = stud[0]
+		passw = stud[1]
+		getdata(idnum, passw)
+	tocsv(database)
